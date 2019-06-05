367c1b9c73a89790bd0ea0a742a307b9711b91a3
nianqingren2008 <295113458@qq.com>
2019-06-05 16:53:00 +0800

    增加 TagClass 的实现类

---
+++ jlpServiceProvider/src/main/java/com/callan/service/provider/service/impl/JTableclassdictServiceImpl.java
@@ -0,0 +1,22 @@
+package com.callan.service.provider.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.callan.service.provider.dao.mapper.JTableclassdictMapper;
+import com.callan.service.provider.pojo.tableclassdict.JTableclassdict;
+import com.callan.service.provider.service.IJTableclassdictService;
+
+public class JTableclassdictServiceImpl implements IJTableclassdictService{
+
+	@Autowired
+	private JTableclassdictMapper  jTableclassdictMapper;
+
+	@Override
+	public List<JTableclassdict> getAll() {
+		 
+		return jTableclassdictMapper.getAll();
+	}
+
+}
